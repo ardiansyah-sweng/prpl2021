65046e2dca8d7b22d3b41c22b023023f73d34519
sendyapriatna <sendyapriatna001@gmail.com>
2021-03-31 13:58:32 +0700

    add new dashboard

--- dashboard.php
+++ dashboard.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['username'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" type="text/css" href="css/cssku.css">
+  <link rel="stylesheet" type="text/css" href="css/cssqu.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
   <link rel="icon" type="icon/png" href="img/lego.ico">
 
