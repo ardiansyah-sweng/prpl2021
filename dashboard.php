72cfa357c35c077ba958cdccb96c89bad4a05f50
isan0880 <ikhsanaditya33@gmail.com>
2021-04-09 20:46:54 +0700

    signup

--- dashboard.php
+++ dashboard.php
@@ -11,7 +11,7 @@ echo "<script>alert('Welcome');</script>, ". $_SESSION['username'];
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>FrontendFunn -</title>
+    <title>Dashboard</title>
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css"
