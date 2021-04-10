0fa29d76769c5fc7651c8c28c963a4adcef5dbed
muhammadsyafiqakmal06 <akmal.indo456@gmail.com>
2021-04-10 10:02:59 +0700

    Muhammad Syafiq Akmal_1900018181

--- dashboard.php
+++ dashboard.php
@@ -4,3 +4,17 @@ if (!isset($_SESSION['username'])) {
     header("Location: signup.php");
 }
 echo 'Welcome, '. $_SESSION['username'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
