f7d50e03368fe27344e31439ff7dae3d640bc014
hanny <hhanny772@gmail.com>
2021-04-11 20:13:20 +0700

    Hanny Handayani Sucinta - Kelas D - 1900018227

--- dashboard.php
+++ dashboard.php
@@ -4,3 +4,48 @@ if (!isset($_SESSION['username'])) {
     header("Location: signup.php");
 }
 echo 'Welcome, '. $_SESSION['username'];
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title>Dashboard Admin</title>
+ <link rel="stylesheet" href="../asset/defaullt.css">
+ <link rel="stylesheet" href="../asset/font/css/all.css" />
+</head>
+<body>
+ <div class="navbar">
+  <h3 class="admin">Dashboard Admin</h3>
+ </div>
+ <div class="sidebar">
+  <div class="nav"></div>
+  <a href=""><button class="btnt"><i style="margin-right: 5px" class="fa fa-home"></i></button><br></a>
+  <a href="insert.php"><button class="btn"><i style="margin-right: 5px" class="fa fa-book"></i></button><br></a>
+  <a href="../../dapur"><button class="btn"><i style="margin-right: 5px" class="fa fa-list"></i></button><br></a>
+  <a href="../../kasir"><button class="btn"><i style="margin-right: 5px" class="fa fa-lock"></i></button><br></a>
+  <a href="../../logout.php"><button class="btnn"><i style="margin-right: 5px" class="fa fa-user"></i></button><br></a>
+ </div>
+ <div class="wrapper">
+  <div class="boxx">
+   <div class="box1">
+    <i class="fa fa-home fa-5x" style="color: white;margin:30px 20px"></i>
+    <div class="title"><h2 class="int">Home</h2></div>
+   </div>
+   <div class="box2">
+    <i class="fa fa-book fa-5x" style="color: white;margin:30px 20px"></i>
+    <div class="title"><h2 class="intt">Menu</h2></div>
+   </div>
+   <div class="box3">
+    <i class="fa fa-list fa-5x" style="color: white;margin:30px 20px"></i>
+    <div class="title"><h2 class="inttt">Dapur</h2></div>
+   </div>
+   <div class="box4">
+    <i class="fa fa-unlock fa-5x" style="color: white;margin:30px 20px"></i>
+    <div class="title"><h2 class="intttt">Kasir</h2></div>
+   </div>
+  </div>
+
+ </div>
+</body>
+</html>
--- signup.php
+++ signup.php
@@ -1,36 +1,82 @@
-<?php
-
-/**
- * SignUp
- * 
- */
-class SignUp
-{
-    /**
-     * Send an email to new user
-     */
-    function sendEmail()
-    {
-        //Your code is here
+<HTML>
+<HEAD>
+<TITLE>User Registration</TITLE>
+<link href="assets/css/phppot-style.css" type="text/css"
+	rel="stylesheet" />
+<link href="assets/css/user-registration.css" type="text/css"
+	rel="stylesheet" />
+<script src="vendor/jquery/jquery-3.3.1.js" type="text/javascript"></script>
+</HEAD>
+<BODY>
+	<div class="phppot-container">
+		<div class="sign-up-container">
+			<div class="login-signup">
+				<a href="index.php">Login</a>
+			</div>
+			<div class="">
+				<form name="sign-up" action="" method="post"
+					onsubmit="return signupValidation()">
+					<div class="signup-heading">Registration</div>
+				<?php
+    if (! empty($registrationResponse["status"])) {
+        ?>
+                    <?php
+        if ($registrationResponse["status"] == "error") {
+            ?>
+				    <div class="server-response error-msg"><?php echo $registrationResponse["message"]; ?></div>
+                    <?php
+        } else if ($registrationResponse["status"] == "success") {
+            ?>
+                    <div class="server-response success-msg"><?php echo $registrationResponse["message"]; ?></div>
+                    <?php
+        }
+        ?>
+				<?php
     }
-
-    function inputValidation($email, $password)
-    {
-        if (empty($email) || empty($password)) {
-            return 'Username or password is empty!';
-        }
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return "Invalid email format!";
-        }
-        if (strlen($password) < 6) {
-            return "Your Password Must Contain At Least 6 Characters!";
-        }
-        if (!preg_match("#[0-9]+#", $password) && !preg_match("#[A-Z]+#", $password)) {
-            return "Your Password Must Contain At Least 1 Number or capital letter! ";
-        }
-        
-        session_start();
-        $_SESSION['username'] = $email;
-        header("location: dashboard.php");
+    ?>
+				<div class="error-msg" id="error-msg"></div>
+					<div class="row">
+						<div class="inline-block">
+							<div class="form-label">
+								Username<span class="required error" id="username-info"></span>
+							</div>
+							<input class="input-box-330" type="text" name="username"
+								id="username">
+						</div>
+					</div>
+					<div class="row">
+						<div class="inline-block">
+							<div class="form-label">
+								Email<span class="required error" id="email-info"></span>
+							</div>
+							<input class="input-box-330" type="email" name="email" id="email">
+						</div>
+					</div>
+					<div class="row">
+						<div class="inline-block">
+							<div class="form-label">
+								Password<span class="required error" id="signup-password-info"></span>
+							</div>
+							<input class="input-box-330" type="password"
+								name="signup-password" id="signup-password">
+						</div>
+					</div>
+					<div class="row">
+						<div class="inline-block">
+							<div class="form-label">
+								Confirm Password<span class="required error"
+									id="confirm-password-info"></span>
+							</div>
+							<input class="input-box-330" type="password"
+								name="confirm-password" id="confirm-password">
+						</div>
+					</div>
+					<div class="row">
+						<input class="btn" type="submit" name="signup-btn"
+							id="signup-btn" value="Sign up">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
     }
-}
