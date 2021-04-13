72bce944c4ab9e62a25e337ec41a4dac75451022
Edo Kurniawan <edok7179@gmail.com>
2021-04-14 02:19:46 +0700

    Upload Pertama Kali Edo Kurniawan 202

---
+++ Signup/cek_login.php
@@ -0,0 +1,21 @@
+<?php
+$email     = $_POST['email'];
+$pass     = $_POST['password'];
+
+include 'koneksi.php';
+
+$cek = mysqli_query($connect, "select * from user  where email='$email' and password='$pass'");
+$data = mysqli_num_rows($cek);
+
+
+if ($data > 0) {
+    session_start();
+    $row = mysqli_fetch_array($cek);
+    $_SESSION['username'] = $email;
+
+    echo "<script>alert('Selamat anda telah berhasil Login');
+    window.location='dashbord.php'</script>";
+} else {
+    echo "<script>alert('Email Dan Password yang anda masukkan salah Silahkan ulangi!');
+    window.location='index.php'</script>";
+}
---
+++ Signup/koneksi.php
@@ -0,0 +1,6 @@
+<?php
+$host           = "localhost";
+$user           = "root";
+$password       = "";
+$database       = "users";
+$connect        = mysqli_connect($host, $user, $password, $database);
