ff8b7b4f84bde92707d37103f6a3185efd41cac3
DennyFajarAlvianto <dennyfajar257@gmail.com>
2021-04-14 18:00:11 +0700

    Escape

---
+++ login.php
@@ -0,0 +1,104 @@
+<?php 
+session_start();
+include_once('koneksi.php');
+$database = new database();
+
+if(isset($_SESSION['is_login']))
+{
+    header('location:dashboard.php');
+}
+
+if(isset($_COOKIE['username']))
+{
+  $database->relogin($_COOKIE['username']);
+  header('location:dashboard.php');
+}
+
+if(isset($_POST['login']))
+{
+    $email = validate($_POST['email']);
+    $password = validate($_POST['password']);
+    if (empty($email)){
+        header("Location: login.php?error=User email is required");
+    exit();
+    }else if(empty($password)){
+        header("Location: login.php?error=Password is required");
+    exit();
+    }else{
+        $password = md5($password); 
+        $sql = "SELECT * FROM users WHERE email='$email' AND pass='$password'";
+
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result)===1){
+            $row = mysqli_fetch_assoc($result);
+
+            if($row['email']===$email && $row['pass']===$password){
+                $_SESSION['email']= $row['email'];
+                $_SESSION['nama']= $row['nama'];
+                $_SESSION['id']= $row['id'];
+                header("Location: dashboard.php");
+            }
+            else{
+                header("Location: login.php?error=Incoreect Username or Password");
+                exit();      
+            }
+        }else{
+            header("Location: login.php?error=Incoreect Username or Password");
+            exit();
+        }
+    }
+}else{
+    header("Location: dashboard.php");
+    exit();
+}
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Login Form</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
+
+    <!-- Bootstrap core CSS -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="assets/css/signin.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+    <form class="form-signin" method="post" action="">
+  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+  <label for="email" class="sr-only">Email</label>
+  <input type="email" id="email" class="form-control" placeholder="Email" name="email" required autofocus><br>
+  <label for="password" class="sr-only">Password</label>
+  <input type="password" id="password" class="form-control" placeholder="Password" name="password" required>
+  <div class="checkbox mb-3">
+  </div>
+  <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Sign in</button>
+  <a href="signup.php" class="btn btn-lg btn-success btn-block">Register</a>
+</form>
+</body>
+</html>
--- signup.php
+++ signup.php
@@ -1,36 +1,107 @@
 <?php
-
-/**
- * SignUp
- * 
- */
-class SignUp
+include_once('koneksi.php');
+$database = new database();
+if(isset($_POST['register']))
 {
-    /**
-     * Send an email to new user
-     */
-    function sendEmail()
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
+    $nama = $_POST['nama'];
+    if($database->register($username,$password,$nama))
     {
-        //Your code is here
+      header('location:login.php');
     }
-
-    function inputValidation($email, $password)
-    {
-        if (empty($email) || empty($password)) {
-            return 'Username or password is empty!';
-        }
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return "Invalid email format!";
-        }
-        if (strlen($password) < 6) {
-            return "Your Password Must Contain At Least 6 Characters!";
-        }
-        if (!preg_match("#[0-9]+#", $password) && !preg_match("#[A-Z]+#", $password)) {
-            return "Your Password Must Contain At Least 1 Number or capital letter! ";
-        }
-        
-        session_start();
-        $_SESSION['username'] = $email;
-        header("location: dashboard.php");
+    if (empty($email) || empty($password)) {
+        return 'Username or password is empty!';
     }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return "Invalid email format!";
+    }
+    if (strlen($password) < 6) {
+        return "Your Password Must Contain At Least 6 Characters!";
+    }
+    if (!preg_match("#[0-9]+#", $password) && !preg_match("#[A-Z]+#", $password)) {
+        return "Your Password Must Contain At Least 1 Number or capital letter! ";
+    }
+    
+    session_start();
+    $_SESSION['email'] = $email;
+    header("location: dashboard.php");
 }
+  
+?>
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Register Form</title>
+ 
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sticky-footer/">
+ 
+    <!-- Bootstrap core CSS -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ 
+ 
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+ 
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer.css" rel="stylesheet">
+  </head>
+  <body class="d-flex flex-column h-100">
+    <!-- Begin page content -->
+<main role="main" class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5">Register Form</h1>
+    <p class="lead">Silahkan Daftarkan Identitas Anda</p>
+    <hr/>
+    <form method="post" action="">
+
+    <div class="form-group row">
+      <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama">
+      </div>
+    </div>
+
+    <div class="form-group row">
+      <label for="email" class="col-sm-2 col-form-label">Email</label>
+      <div class="col-sm-10">
+        <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+      </div>
+    </div>
+ 
+  <div class="form-group row">
+    <label for="password" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <a href="login.php" class="btn btn-success">Login</a>
+      <button type="submit" class="btn btn-primary" name="register">Register</button>
+    </div>
+  </div>
+</form>
+  </div>
+</main>
+
+</body>
+</html>
