4e0cd47d1e72f5a2ab2bc5e93800fb0c4aac97a2
lintangfaa <lintang1900018258@webmail.uad.ac.id>
2021-04-14 12:23:32 +0700

    Try2

--- dashboard.php
+++ dashboard.php
@@ -4,3 +4,5 @@ if (!isset($_SESSION['username'])) {
     header("Location: signup.php");
 }
 echo 'Welcome, '. $_SESSION['username'];
+
+<a href="signout.php"><input class="logout" type="submit" value="Sign Out" name="logout"></a>
