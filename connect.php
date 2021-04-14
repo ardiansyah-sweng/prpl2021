78266faff575ccb1d10af85493185c91b7b01f5b
dickysyahrio <dickysyahrio1310@gmail.com>
2021-04-14 13:47:25 +0700

    prpl

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php 
+  try 
+      { 
+           $koneksi = new PDO('mysql:host=localhost;dbname=user', 'root', '', array(PDO::ATTR_PERSISTENT => true)); 
+      } 
+      catch(PDOException $e) 
+      { 
+           echo $e->getMessage(); 
+      } 
+      include_once 'function.php'; 
+      $user = new objek($koneksi); 
+ ?>
