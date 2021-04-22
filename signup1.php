768c781f7aa0f933087be6a8dda426e715411a38
omaulidya <omaulidya@gmail.com>
2021-04-22 23:14:22 +0800

    telah membuat signup versi saya

--- signup1.php
+++ signup1.php
@@ -1,16 +1,6 @@
 <?php
-
-/**
- * SignUp
- * 
- */
 class SignUp
 {
-    /**
-     * Send an email to new user
-     */
-    function sendEmail()
-    {
 <HTML>
 <HEAD>
 <TITLE>User Registration</TITLE>
@@ -92,25 +82,4 @@ class SignUp
 			</div>
 		</div>
 	</div>
-    }
-
-    function inputValidation($email, $password)
-    {
-        if (empty($email) || empty($password)) {
-            return 'Username or password is empty!';
-        }
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return "Invalid email format!";
-        }
-        if (strlen($password) < 6) {
-            return "Your Password Must Contain At Least 6 Characters!";
-        }
-        if (!preg_match("#[0-9]+#", $password) && !preg_match("#[A-Z]+#", $password)) {
-            return "Your Password Must Contain At Least 1 Number or capital letter! ";
-        }
-        
-        session_start();
-        $_SESSION['username'] = $email;
-        header("location: dashboard.php");
-    }
 }
