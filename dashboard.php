cfdd20b13b86ae375e0f91c6d93401297b468e2f
BueLat <bsultanramadan@gmail.com>
2021-05-05 23:54:44 +0700

    Menambahkan file dashboard.php, config.php, dashboard.css, style.css dan menambahkan fitur sendEmail serta logic pada inputValidation.

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,49 @@
 <?php
+
 session_start();
 if (!isset($_SESSION['username'])) {
     header("Location: signup.php");
 }
-echo 'Welcome, '. $_SESSION['username'];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/dashboard.css">
+
+    <title>Dashboard - buelat.</title>
+</head>
+<body>
+
+    <header>
+      <div class="head-container">
+        <div class="head-logo">
+           <a href="#" class="head-brand">
+             <img src="image/logo.png" alt="">
+           </a>
+        </div>
+        <div class="nav">
+            <a href="#" class="nav-item active">Home</a>
+            <a href="#" class="nav-item">Features</a>
+            <a href="#" class="nav-item">Contact</a>
+            <a href="logout.php" class="nav-item logout">Logout</a>
+        </div>
+      </div>
+    </header>
+
+    <section>
+        <div class="content">
+            <h1>Welcome,</h1>
+            <p><?= $_SESSION['username'] ?></p>
+        </div>
+    </section>
+    
+</body>
+</html>
