5ed44e86fb1233143e778275f5afdad9eef41377
omaulidya <omaulidya@gmail.com>
2021-05-06 06:42:10 +0800

    perubahan

--- login1.php
+++ login1.php
@@ -1,19 +1,20 @@
 <?php include 'koneksi.php'; ?>
-<!DOCTYPE html>
-<HTML>
-<HEAD>
-<TITLE>Sign Up</TITLE>
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Login</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/style.css">
-</HEAD>
-
-<BODY>
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">SignUp</h2>
+					<h2 class="heading-section">Login</h2>
 				</div>
 			</div>
 			<div class="row justify-content-center">
@@ -22,6 +23,7 @@
 		      	<div class="icon d-flex align-items-center justify-content-center">
 		      		<span class="fa fa-user-o"></span>
 		      	</div>
+		      	<h3 class="text-center mb-4">Have an account?</h3>
 						<form action="#" class="login-form">
 		      		<div class="form-group">
 		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
@@ -29,22 +31,36 @@
 	            <div class="form-group d-flex">
 	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
 	            </div>
-
+	            <div class="form-group d-md-flex">
+	            	<div class="w-50">
+	            		<label class="checkbox-wrap checkbox-primary">Remember Me
+									  <input type="checkbox" checked>
+									  <span class="checkmark"></span>
+									</label>
+								</div>
+								<div class="w-50 text-md-right">
+									<a href="#">Forgot Password</a>
+								</div>
+	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">SIGN UP</button>
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
 	            </div>
 	          </form>
 	        </div>
 				</div>
 			</div>
+		</div>
+	</section>
 
-<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.min.js"></script>
   <script src="js/popper.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script src="js/main.js"></script>
-  </BODY>
-</HTML>
-  <?php
+
+	</body>
+</html>
+
+<?php
     function inputValidation($username, $password)
     {
         if (empty($username) || empty($password)) {
