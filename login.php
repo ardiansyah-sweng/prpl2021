37a3607b6622cb2ad9ba2a99807c0183c8b7d994
HaniyahAzzah291 <haniyahazah@gmail.com>
2021-05-06 22:50:29 +0700

    uts_prpl

--- login.php
+++ login.php
@@ -1,53 +1,23 @@
-<?php 
-session_start(); 
-include "db_conn.php";
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+     <form action="login.php" method="post">
+     	<h2>LOGIN</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+     	<label>User Name</label>
+     	<input type="text" name="uname" placeholder="User Name"><br>
 
-if (isset($_POST['e-mail']) && isset($_POST['password'])) {
+     	<label>Password</label>
+     	<input type="password" name="password" placeholder="Password"><br>
 
-	function validate($data){
-       $data = trim($data);
-	   $data = stripslashes($data);
-	   $data = htmlspecialchars($data);
-	   return $data;
-	}
-
-	$email = validate($_POST['e-mail']);
-	$pass = validate($_POST['password']);
-
-	if (empty($email)) {
-		header("Location: index.php?error=User Name is required");
-	    exit();
-	}else if(empty($pass)){
-        header("Location: index.php?error=Password is required");
-	    exit();
-	}else{
-		// hashing the password
-        $pass = md5($pass);
-
-        
-		$sql = "SELECT * FROM users WHERE e-mail='$email' AND password='$pass'";
-
-		$result = mysqli_query($conn, $sql);
-
-		if (mysqli_num_rows($result) === 1) {
-			$row = mysqli_fetch_assoc($result);
-            if ($row['e-mail'] === $email && $row['password'] === $pass) {
-            	$_SESSION['e-mail'] = $row['user_name'];
-            	$_SESSION['name'] = $row['name'];
-            	$_SESSION['id'] = $row['id'];
-            	header("Location: home.php");
-		        exit();
-            }else{
-				header("Location: index.php?error=Incorect User name or password");
-		        exit();
-			}
-		}else{
-			header("Location: index.php?error=Incorect User name or password");
-	        exit();
-		}
-	}
-	
-}else{
-	header("Location: index.php");
-	exit();
-}
+     	<button type="submit">Login</button>
+          <a href="signup.php" class="ca">Create an account</a>
+     </form>
+</body>
+</html>
