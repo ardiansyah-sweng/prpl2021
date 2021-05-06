27a2c0ba2ac178fefcf0d8d672f3e9cabf99ce95
Kholifatush <62455275+Kholifatush@users.noreply.github.com>
2021-05-06 17:48:18 +0700

    Update Api_data_covid.php

--- UTS No 6_Program data dari Api Publik menggunakan restfull Api/Api_data_covid.php
+++ UTS No 6_Program data dari Api Publik menggunakan restfull Api/Api_data_covid.php
@@ -1,13 +1,4 @@
 <?php
-
- /** 
- * 
- * Code example; for more information see:
- * https://oneguyoneblog.com/2020/03/19/covid-19-corona-get-data-with-php/
- * 
- * 
- **/
-
  $url = "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?where=UPPER(Country_Region)%20like%20%27%25INDONESIA%25%27&outFields=Last_Update,Recovered,Deaths,Confirmed&returnGeometry=false&outSR=4326&f=json";
  
  $client = curl_init($url);
