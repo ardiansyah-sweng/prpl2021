b7bbc3be732bb35a3cbe1b830f334421d686064b
HannySucinta <hanny1900018227@webmail.uad.ac.id>
2021-05-06 23:27:07 +0700

    Hanny Handayani Sucinta_D_1900018227

--- Login.php
+++ Login.php
@@ -1,61 +1,40 @@
-<?php
-class Login
-{
-    function sendEmail()
-    {
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>Yeah Login</title>
-
-<!-- bootstrap-3.3.7 -->
-<link rel="stylesheet" href="bootstrap-3.3.7/css/bootstrap.min.css">
-<script src="bootstrap-3.3.7/js/bootstrap.min.js"></script>
-
-<!-- JQUERY -->
-<script type="text/javascript" language="javascript" src="jquery/jquery.js"></script>
-
-<link href="style/style.css" rel="stylesheet" type="text/css" media="all"/>
-<script type="text/javascript" language="javascript" src="style/style.js"></script>
-
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/stylereg.css">
+    <title>Document</title>
 </head>
 <body>
+<div id="card">
+<div id="card-content">
+  <div id="card-title">
+    <h2>Login Here! </h2>
+        <div class="underline-title"></div>
+        <form class="form" action="login.php" method="post" enctype="multipart/form-data" >
 
-<div class="container">
-        <div class="card card-container">
-            <img id="profile-img" class="profile-img-card" src="img/avatar_2x.png" />
-            <p id="profile-name" class="profile-name-card">Suckittrees.Com</p>
-            <form class="form-signin" action="" method="POST">
-                <span id="reauth-email" class="reauth-email"></span>
-                <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required autofocus>
-                <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
-                <br>
-                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit"  name="login">Sign in</button>
-            </form>
-            
-        </div>
+    <table>
+        <tr>
+            <td>Nama</td>
+            <td>:</td>
+            <td> <input type="text" name="email" value=""> </td>
+
+        </tr>
+        <tr>
+            <td>Password </td>
+            <td>:</td>
+            <td> <input type="password" name="password" value=""> </td>
+
+        </tr>
+    </table>
+    <button type="submit" name="submit" >Daftar</button>
+</form>
+         </div>
+    </div> 
 </div>
-
 </body>
 </html>
-<?php
-include "db_con.php";
-IF(ISSET($_POST['login'])){
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    
-    $cek = mysql_num_rows(mysql_query("SELECT * FROM user_login WHERE email='$email' AND password='$password'"));
-    $data = mysql_fetch_array(mysql_query("SELECT * FROM user_login WHERE email='$email' AND password='$password'"));
-    IF($cek > 0)
-    {
-        
-        session_start();
-        $_SESSION['email'] = $data['email'];
-        $_SESSION['name'] = $data['full_name'];
-        echo "<script language=\"javascript\">alert(\"welcome \");document.location.href='index.php';</script>";
-    }else{
-        echo "<script language=\"javascript\">alert(\"Invalid username or password\");document.location.href='login.php';</script>";
-    }
-}
-}
-?>
--- signup.php
+++ signup.php
@@ -1,82 +1,124 @@
-<HTML>
-<HEAD>
-<TITLE>User Registration</TITLE>
-<link href="assets/css/phppot-style.css" type="text/css"
-	rel="stylesheet" />
-<link href="assets/css/user-registration.css" type="text/css"
-	rel="stylesheet" />
-<script src="vendor/jquery/jquery-3.3.1.js" type="text/javascript"></script>
-</HEAD>
-<BODY>
-	<div class="phppot-container">
-		<div class="sign-up-container">
-			<div class="login-signup">
-				<a href="index.php">Login</a>
-			</div>
-			<div class="">
-				<form name="sign-up" action="" method="post"
-					onsubmit="return signupValidation()">
-					<div class="signup-heading">Registration</div>
-				<?php
-    if (! empty($registrationResponse["status"])) {
-        ?>
-                    <?php
-        if ($registrationResponse["status"] == "error") {
-            ?>
-				    <div class="server-response error-msg"><?php echo $registrationResponse["message"]; ?></div>
-                    <?php
-        } else if ($registrationResponse["status"] == "success") {
-            ?>
-                    <div class="server-response success-msg"><?php echo $registrationResponse["message"]; ?></div>
-                    <?php
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+
+class masuk{
+    public $username="";
+    public $password="";
+
+    public function register()
+    {
+//////////////////
+    //Instantiation and passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'hansu212223@gmail.com';                     //SMTP username
+    $mail->Password   = 'Lalaland7071';                               //SMTP password
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('hansu212223@gmail.com', 'Admin');
+    $mail->addAddress($email);     //Add a recipient
+    $mail->addReplyTo('hansu212223@gmail.com', 'Admin');
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Akun Anda telah aktif!';
+    $mail->Body    = '<h1>Congratulations!!</h1> <p>Welcome to our newest member!</p>';
+
+    if($mail->send())
+    {
+        echo 'Message has been sent';
+    }
+    else{
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+///////////
+    }
+    
+    public function checkout()
+    {
+        $koneksi = mysqli_connect('localhost', 'root', '', 'db_user') or die($koneksi);
+        if (empty($this->email) || empty($this->password)) {
+            echo "email or password is empty!";
         }
-        ?>
-				<?php
+        
+        if (strlen($this->password) < 6) {
+            echo "Your Password Must Contain At Least 6 Characters!";
+        }
+        if (!preg_match("#[0-9]+#", $this->password) && !preg_match("#[A-Z]+#", $this->password)) {
+            echo "Your Password Must Contain At Least 1 Number or capital letter! ";
+        }
+        session_start();
+        $_SESSION['email'] = $this->email;
+    
+        if(!empty($this->email)and !empty($this->password)){$sql_insert = "INSERT INTO user VALUES('$this->email','$this->password')";
+            mysqli_query($koneksi,$sql_insert);
+                header("location:dashboard.php"); 
+        }
     }
-    ?>
-				<div class="error-msg" id="error-msg"></div>
-					<div class="row">
-						<div class="inline-block">
-							<div class="form-label">
-								Username<span class="required error" id="username-info"></span>
-							</div>
-							<input class="input-box-330" type="text" name="username"
-								id="username">
-						</div>
-					</div>
-					<div class="row">
-						<div class="inline-block">
-							<div class="form-label">
-								Email<span class="required error" id="email-info"></span>
-							</div>
-							<input class="input-box-330" type="email" name="email" id="email">
-						</div>
-					</div>
-					<div class="row">
-						<div class="inline-block">
-							<div class="form-label">
-								Password<span class="required error" id="signup-password-info"></span>
-							</div>
-							<input class="input-box-330" type="password"
-								name="signup-password" id="signup-password">
-						</div>
-					</div>
-					<div class="row">
-						<div class="inline-block">
-							<div class="form-label">
-								Confirm Password<span class="required error"
-									id="confirm-password-info"></span>
-							</div>
-							<input class="input-box-330" type="password"
-								name="confirm-password" id="confirm-password">
-						</div>
-					</div>
-					<div class="row">
-						<input class="btn" type="submit" name="signup-btn"
-							id="signup-btn" value="Sign up">
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-    }
+}
+$in = new masuk();
+if(isset($_POST['submit'])){
+    $in->email = $_POST['email'];
+    $in->password = $_POST['password'];
+    $in -> register();
+    $in -> checkout();
+    
+    
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sign up</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/stylereg.css">
+    <title>Document</title>
+</head>
+<body>
+<div id="card">
+<div id="card-content">
+  <div id="card-title">
+    <h2>Sign up Here! </h2>
+        <div class="underline-title"></div>
+        <form class="form" action="signup.php" method="post" enctype="multipart/form-data" >
+
+    <table>
+        <tr>
+            <td>Nama</td>
+            <td>:</td>
+            <td> <input type="text" name="email" value=""> </td>
+
+        </tr>
+        <tr>
+            <td>Password </td>
+            <td>:</td>
+            <td> <input type="password" name="password" value=""> </td>
+
+        </tr>
+    </table>
+    <button type="submit" name="submit" >Daftar</button>
+    <p>Sudah punya akun? <a style="text-decoration : none" href="login.php">Login disini!</a></p>
+</form>
+         </div>
+    </div> 
+</div>
+</body>
+</html>
