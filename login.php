def878cbc522ecaf4c26071a40748b9d8f80e2eb
DennyFajarAlvianto <80819138+DennyFajarAlvianto@users.noreply.github.com>
2021-05-06 21:12:54 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php 
+    session_start();
+    include 'koneksi.php';
+ 
+    // menangkap data yang dikirim dari form login
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+ 
+    // menyeleksi data pada tabel admin dengan username dan password yang sesuai
+    $data = mysqli_query($koneksi, "SELECT * FROM tb_user WHERE email='$email' and pass='$pass'");
+ 
+    // menghitung jumlah data yang ditemukan
+    $cek = mysqli_num_rows($data);
+ 
+    if($cek > 0){
+        $_SESSION['email'] = $email;
+        $_SESSION['status'] = "login";
+        header("location:dashboard.php");
+    }
+    else{
+        header("location:index.php?pesan=gagal");
+    }
+?>
