f64df75a6848c3c9a61b3bb413b8a552487ead26
nandifasatriap <nandifasatria@gmail.com>
2021-05-06 21:59:26 +0700

    prpl

---
+++ no6_API_kontak.php
@@ -0,0 +1,29 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
+
+class Kontak extends REST_Controller {
+
+    function __construct($config = 'rest') {
+        parent::__construct($config);
+        $this->load->database();
+    }
+
+    //Menampilkan data kontak
+    function index_get() {
+        $id = $this->get('id');
+        if ($id == '') {
+            $kontak = $this->db->get('telepon')->result();
+        } else {
+            $this->db->where('id', $id);
+            $kontak = $this->db->get('telepon')->result();
+        }
+        $this->response($kontak, 200);
+    }
+
+
+}
+?>
