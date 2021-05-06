fee4aef29bd39805de96c59a98e6710fe5dd008f
andre-pramafujiara <62455161+andre-pramafujiara@users.noreply.github.com>
2021-05-06 11:14:18 +0700

    Add files via upload

---
+++ hasilregis.php
@@ -0,0 +1,17 @@
+
+<html>
+  <head>
+   
+
+    <!-- Home -->
+    <section id="home" >
+        <h1>Register Berhasil</h1>   
+       
+    </section>  
+    <!-- End Home -->
+    <p>
+                Segera Login Akun Anda
+                <a href="signin.php">klik untuk Login</a>
+            </p>
+  </body>
+</html>
